8a118f5bd86d2390381ea0df274a8712b48c9de5
Mirage <clementbobin21@gmail.com>
2024-11-07 20:02:17 +0100

    Update setup.sh
    Signed-off-by: Mirage <clementbobin21@gmail.com>

--- infra/setup.sh
+++ infra/setup.sh
@@ -2,10 +2,10 @@
 
 # Start the Docker container in detached mode
 echo "Starting the Docker container..."
-docker-compose -f ./infra/docker-compose-dev.yml up -d
+docker-compose -f ./infra/docker-compose-test.yml up -d
 
 # Confirm the container is running
 echo "Checking container status..."
-docker-compose -f ./infra/docker-compose-dev.yml ps
+docker-compose -f ./infra/docker-compose-test.yml ps
 
 echo "All tasks completed successfully."
