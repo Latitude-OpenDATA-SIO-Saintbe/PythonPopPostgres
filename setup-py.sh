35e9b3d3716d3814cea2a39b8d691ac5553ed8e1
Mirage <clementbobin21@gmail.com>
2024-11-08 15:11:28 +0100

    Update setup-py.sh
    Signed-off-by: Mirage <clementbobin21@gmail.com>

--- setup-py.sh
+++ setup-py.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Define the desired path
+BASE_PATH="$1"
+
+# Check if a path was provided
+if [ -z "$BASE_PATH" ]; then
+  echo "Usage: $0 <path>"
+  exit 1
+fi
+
 # List of Python scripts to run
 python_scripts=("DB-create.py" "DB-fake-seed.py")  # Add your Python scripts here
 
@@ -18,7 +27,7 @@ fi
 # Step 3: Loop through the list of Python scripts and run each one
 for script in "${python_scripts[@]}"; do
     echo "Running $script..."
-    python "db-seed/$script"
+    python "$BASE_PATH/$script"
     
     # Check if the Python script ran successfully
     if [ $? -eq 0 ]; then
